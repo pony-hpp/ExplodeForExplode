5f2f6c84cfd04547e0d710aad4734649455e9ec0
pony-hpp <pony.hpp@outlook.com>
2024-10-06 21:12:01 +0300

    test: Add tests for OpenGL maths

---
+++ tests/src/opengl/math/matrix.cpp
@@ -0,0 +1,49 @@
+#include "opengl/math/matrix.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace gl::math;
+
+static void _assert_matrices(const Matrix &mat1, const Matrix &mat2) noexcept {
+  for (unsigned char i = 0; i < 16; i++) {
+    ASSERT_NEAR(mat1[i], mat2[i], 0.0001f);
+  }
+}
+
+TEST(GlMath, ProjectionMatrixTest) {
+  _assert_matrices(
+    projection_matrix(40, 40),
+    {
+      // clang-format off
+      0.05f, 0.0f,  0.0f, -1.0f,
+      0.0f,  0.05f, 0.0f, -1.0f,
+      0.0f,  0.0f,  1.0f,  0.0f,
+      0.0f,  0.0f,  0.0f,  1.0f
+      // clang-format on
+    }
+  );
+
+  _assert_matrices(
+    projection_matrix(800, 600),
+    {
+      // clang-format off
+      0.0025f, 0.0f,   0.0f, -1.0f,
+      0.0f,   0.0033f, 0.0f, -1.0f,
+      0.0f,   0.0f,    1.0f,  0.0f,
+      0.0f,   0.0f,    0.0f,  1.0f
+      // clang-format on
+    }
+  );
+
+  _assert_matrices(
+    projection_matrix(750, 1334),
+    {
+      // clang-format off
+      0.0026f, 0.0f,  0.0f, -1.0f,
+      0.0f,  0.0014f, 0.0f, -1.0f,
+      0.0f,  0.0f,  1.0f,  0.0f,
+      0.0f,  0.0f,  0.0f,  1.0f
+      // clang-format on
+    }
+  );
+}
